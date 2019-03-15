5c46ab4f9a73ec081f49d3241a65149d04fd56c5
Jan Wiegmann <wiegmann@powerfolder.com>
2019-03-15 10:35:01 +0100

    Implemented service methods // Added copy constructors to to TodoFull for TodoBase and TodoList models

--- src/main/java/io/swagger/model/TodoFull.java
+++ src/main/java/io/swagger/model/TodoFull.java
@@ -1,15 +1,15 @@
 package io.swagger.model;
 
-import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import io.swagger.model.TodoBase;
-import java.time.OffsetDateTime;
 import org.springframework.validation.annotation.Validated;
-import javax.validation.Valid;
-import javax.validation.constraints.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 /**
  * The full todo with identifier used as response object.
@@ -17,72 +17,96 @@ import javax.validation.constraints.*;
 @ApiModel(description = "The full todo with identifier used as response object.")
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-03-14T13:19:45.394Z")
+@Entity
+@Table(name = "todos")
+public class TodoFull extends TodoBase {
 
-public class TodoFull extends TodoBase  {
-  @JsonProperty("id")
-  private Integer id = null;
+    @JsonProperty("id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id = null;
 
-  public TodoFull id(Integer id) {
-    this.id = id;
-    return this;
-  }
+    /**
+     * Copy constructor: TodoBase to TodoFull.
+     *
+     * @param id
+     *
+     * @return
+     */
 
-  /**
-   * Get id
-   * minimum: 0
-   * maximum: 1000000
-   * @return id
-  **/
-  @ApiModelProperty(required = true, value = "")
-  @NotNull
+    public TodoFull(TodoBase todoBase) {
 
-  @Min(0) @Max(1000000)
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
+        super.setDescription(todoBase.getDescription());
+        super.setDueDate(todoBase.getDueDate());
+        super.setTitle(todoBase.getTitle());
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+
+    public TodoFull(TodoList todoList) {
+        super.setDueDate(todoList.getDueDate());
+        super.setTitle(todoList.getTitle());
     }
-    TodoFull todoFull = (TodoFull) o;
-    return Objects.equals(this.id, todoFull.id) &&
-        super.equals(o);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, super.hashCode());
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class TodoFull {\n");
-    sb.append("    ").append(toIndentedString(super.toString())).append("\n");
-    sb.append("    id: ").append(toIndentedString(id)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
+    public TodoFull id(Integer id) {
+        this.id = id;
+        return this;
     }
-    return o.toString().replace("\n", "\n    ");
-  }
+
+    /**
+     * Get id
+     * minimum: 0
+     * maximum: 1000000
+     *
+     * @return id
+     **/
+    @ApiModelProperty(required = true, value = "")
+    @NotNull
+
+    @Min(0)
+    @Max(1000000)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TodoFull todoFull = (TodoFull) o;
+        return Objects.equals(this.id, todoFull.id) && super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, super.hashCode());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class TodoFull {\n");
+        sb.append("    ").append(toIndentedString(super.toString())).append("\n");
+        sb.append("    id: ").append(toIndentedString(id)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
 }
 
